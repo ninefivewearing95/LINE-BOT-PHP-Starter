7c68da8b6bce8ce926f88b6814952a6745cbe982
ninefivewearing95 <ninefive02@gmail.com>
2017-07-11 14:14:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,14 +18,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				{
 				'type' => 'text',
-				'text' => อะไรหว่า
-				},
-     				{
-				'type' => 'text',
-				'text' => อะไรวะ
-				}
+				'text' => เก้า ห้า แวริ่ง สวัสดีค่ะ
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
