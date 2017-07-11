c8043985e5d817a43492141a50bca3b941a6bc74
ninefivewearing95 <ninefive02@gmail.com>
2017-07-11 11:34:43 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,16 +17,16 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				{
+			"messages":[
+        {
             "type":"text",
             "text":"Hello, user"
         },
-				{
-            "type":"text1",
-            "text":"Hello1, user1"
-        },
-			];
+        {
+            "type":"text",
+            "text":"May I help you?"
+        }
+    ]
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
