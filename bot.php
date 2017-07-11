ce74390b01f44cd1406455852c09b59b9c54c05c
ninefivewearing95 <ninefive02@gmail.com>
2017-07-11 14:10:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,8 +18,14 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
+				{
 				'type' => 'text',
 				'text' => อะไรหว่า
+				},
+     				{
+				'type' => 'text',
+				'text' => อะไรวะ
+				}
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
