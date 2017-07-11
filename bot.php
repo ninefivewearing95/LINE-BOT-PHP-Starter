869677330124c28a45d478f43670a52eb8bd7c0b
ninefivewearing95 <ninefive02@gmail.com>
2017-07-11 14:07:21 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,8 +18,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				"type":"text",
-            			"text":"May I help you?"
+				'type' => 'text',
+				'text' => Nine Five Wearing Co.,Ltd
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
