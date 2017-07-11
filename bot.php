162322e629ab04729d19288314fab52d5934c051
ninefivewearing95 <ninefive02@gmail.com>
2017-07-11 11:02:58 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -26,9 +26,9 @@ if (!is_null($events['events'])) {
 			];
 			
 			{
-            "type":"text",
-            "text":"May I help you?"
-        }
+          		  "type":"text",
+          		  "text":"May I help you?"
+       			 }
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
