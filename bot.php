7eb9882028f05fb5f52f14b4dc58c048296af067
ninefivewearing95 <ninefive02@gmail.com>
2017-07-11 11:27:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -45,21 +45,3 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
-
-
-curl -X POST \
--H 'Content-Type:application/json' \
--H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
--d '{
-    "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-    "messages":[
-        {
-            "type":"text",
-            "text":"Hello, user"
-        },
-        {
-            "type":"text",
-            "text":"May I help you?"
-        }
-    ]
-}' https://api.line.me/v2/bot/message/reply
